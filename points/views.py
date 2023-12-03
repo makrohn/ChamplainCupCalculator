6523f903e4e5e15bab619ff8ff3b42dcfc100ad3
Matthew Krohn <makrohn@gmail.com>
2023-12-03 07:35:05 -0500

    Handle Youth events in a separate table

--- points/views.py
+++ points/views.py
@@ -6,13 +6,22 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Points,Event,Fencer
 
-def get_standings(weapon):
+def get_standings(weapon, age_limit):
     """Get the standings, along with all points earned, for each fencer for a weapon"""
     points = Points.objects.order_by("competitor_id")
 
     fencers = Fencer.objects.all()
 
-    events = Event.objects.filter(event_name__contains=weapon).order_by("event_date")
+    if age_limit == "adult":
+        events = Event.objects.filter(
+            event_name__contains=weapon).exclude(
+                event_name__contains="Y12").order_by("event_date")
+    elif age_limit == "youth":
+        events = Event.objects.filter(
+                event_name__contains=weapon
+            ).filter(
+                event_name__contains="Y12"
+            ).order_by("event_date")
 
     weapon_events = {}
     cell_id = 1
@@ -33,10 +42,20 @@ def get_standings(weapon):
         row[fencer_name] = {}
         total = 0
         row[fencer_name]['points'] = {}
-        points_earned = Points.objects.filter(
-            competitor_id=fencer,
-            event_placed__event_name__contains=weapon
-            )
+        if age_limit == "adult":
+            points_earned = Points.objects.filter(
+                    competitor_id=fencer,
+                    event_placed__event_name__contains=weapon,
+                ).exclude(
+                    event_placed__event_name__contains="Y12"
+                )
+        elif age_limit == "youth":
+            points_earned = Points.objects.filter(
+                    competitor_id=fencer,
+                    event_placed__event_name__contains=weapon
+                ).filter(
+                    event_placed__event_name__contains="Y12"
+                )
         for entry in points_earned:
             event_name = entry.event_placed.event_name
             event_id = entry.event_placed.event_id
@@ -64,14 +83,17 @@ def index(request):
     """Return several tables with standings for each weapon"""
     context = {}
 
-    epee_standings = get_standings("Epee")
+    epee_standings = get_standings("Epee", "adult")
     context['epee'] = epee_standings
 
-    foil_standings = get_standings("Foil")
+    foil_standings = get_standings("Foil", "adult")
     context['foil'] = foil_standings
 
-    saber_standings = get_standings("Saber")
+    saber_standings = get_standings("Saber", "adult")
     context['saber'] = saber_standings
+
+    youth_foil_standings = get_standings("Foil", "youth")
+    context['youth_foil'] = youth_foil_standings
 
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
